abc4770ef604b8b81e3d9cffdaf8898e34e598f4
hasal314 <hasal314@student.otago.ac.nz>
2023-03-14 14:38:07 +1300

    Fix comments in the FilterActions file.

--- src/cosc202/andie/FilterActions.java
+++ src/cosc202/andie/FilterActions.java
@@ -114,12 +114,37 @@ public class FilterActions {
         }
 
     }
-
+    /**
+     * <p>
+     * Action to blur an image with a sharpen filter.
+     * </p>
+     * 
+     * @see MeanFilter
+     */
     public class SharpenFilterAction extends ImageAction {
+
+        /**
+         * <p>
+         * Create a new mean-filter action.
+         * </p>
+         * 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
         SharpenFilterAction(String name, ImageIcon icon,
             String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
+
+        /**
+         * <p>
+         * This method is called whenever the SharpenFilterAction is triggered.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
         public void actionPerformed(ActionEvent e) {
             // Create and apply the filter
             target.getImage().apply(new SharpenFilter());
