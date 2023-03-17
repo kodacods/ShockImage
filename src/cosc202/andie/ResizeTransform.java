9205100ec65234394f053c9b10c930c568972b8e
Meg Albarico <albme224@student.otago.ac.nz>
2023-03-17 14:50:04 +1300

    Got resize working, figuring custom scale factor

--- src/cosc202/andie/ResizeTransform.java
+++ src/cosc202/andie/ResizeTransform.java
@@ -1,8 +1,12 @@
 package cosc202.andie;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.*;
 
-public class ResizeTransform implements ImageOperation, java.io.Serializable { //this should probably be the easiest one?
+public class ResizeTransform implements ImageOperation, java.io.Serializable { //this should probably be the easiest one? <-- NO lol
+    //i refuse to use graphics 2d
+    //set to a constant scaling factor but provide a custom option lator
     public ResizeTransform(){
     }
     public BufferedImage apply(BufferedImage input) { 
@@ -10,17 +14,16 @@ public class ResizeTransform implements ImageOperation, java.io.Serializable { /
          * Attempt to do with a specific height and size
          * before going into custom selection like in MeanFilter.
          */
-        //problem is it only works on image instances. not buffered?
-        //Process:
-        //Make copy of BufferedImage as an Image
-        //Do that scaling thing on *that* Image
-        //then have it as a BufferedImage output.
-        //But HOW AAAAUUUUUUUGGGGGH
+        double scaleFactor = 2;
+        int newWidth = (int)(input.getWidth() * scaleFactor);
+        int newHeight = (int)(input.getHeight() * scaleFactor);
 
-        
+        Image out = input.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        // okay fine whatever
+        BufferedImage output = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_4BYTE_ABGR); //oh. yeha there we go
+        Graphics2D lay = output.createGraphics();
+        lay.drawImage(out, 0, 0, null);
 
-        // Image result = input.getScaledInstance(2*input.getWidth(), 2*input.getHeight(), Image.SCALE_AREA_AVERAGING);
-        // BufferedImage output = (BufferedImage)result;
-        return input;
+        return output;
     }
 }
