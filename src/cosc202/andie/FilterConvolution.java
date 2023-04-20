ef03bb664bedada20f2dfbb6e49c6eef8933c44a
hasal314 <hasal314@student.otago.ac.nz>
2023-04-20 23:07:35 +1200

    Change Mean & Sharpen filters to use FilterConvolution
    Changed filterconvolution so that it takes any values over 255 or below zero, and rounds them to the nearest valid value.

--- src/cosc202/andie/FilterConvolution.java
+++ src/cosc202/andie/FilterConvolution.java
@@ -31,6 +31,18 @@ public class FilterConvolution {
                         b += db;
                     }
                 }
+                if (r > 255)
+                    r = 255;
+                if (g > 255)
+                    g = 255;
+                if (b > 255)
+                    b = 255;
+                if (r < 0)
+                    r = 0;
+                if (g < 0)
+                    g = 0;
+                if (b < 0)
+                    b = 0;
                 Color outputColor = new Color(Math.round(r), Math.round(g), Math.round(b));
 
                 output.setRGB(x, y, outputColor.getRGB());
