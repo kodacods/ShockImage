347aab370ca939228f9e0cea3079592d7a444091
albme224 <albme224@student.otago.ac.nz>
2023-04-21 21:32:41 +1200

    pain

--- src/cosc202/andie/SelectionActions.java
+++ src/cosc202/andie/SelectionActions.java
@@ -3,6 +3,7 @@ package cosc202.andie;
 import java.util.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.Point;
 
 import java.util.prefs.Preferences;
 
@@ -14,6 +15,11 @@ public class SelectionActions {
      * Create a set of Selection menu actions.
      * </p>
      */
+
+    protected static Point selFirst;
+    protected static Point selSecond;
+    protected static boolean isSelected = false;
+
     public SelectionActions() {
 
     Preferences prefs = Preferences.userNodeForPackage(Andie.class);
@@ -43,5 +49,42 @@ public class SelectionActions {
         }
 
         return selectMenu;
+    }
+
+    public class RectangularSelectionAction extends ImageAction implements MouseListener{
+
+        private Point first;
+        private Point second;
+        private boolean firstCheck;
+
+        RectangularSelectionAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+            target.addMouseListener(this);
+        }
+
+        public void mouseEntered(MouseEvent e) { 
+        }  
+        public void mouseExited(MouseEvent e) {  
+        }  
+        public void mousePressed(MouseEvent e) {  //drag-click looks a lot easier now, just as easy as 2-clicking
+        }  
+        public void mouseReleased(MouseEvent e) {  
+        }  
+        public void mouseClicked(MouseEvent e) {  
+            if(firstCheck == false){
+                first = e.getPoint();
+                firstCheck = true;
+            } else {
+                second = e.getPoint();
+            }
+        }  
+
+        public void actionPerformed(ActionEvent e){
+            
+        }
+
+        /* Steps:
+         * 01: Make panel informing of selection state, how many times clicked, 
+         */
     }
 }
