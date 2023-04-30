6b919ae0d2b8ff58c361dba05a5f90e82f319094
albme224 <albme224@student.otago.ac.nz>
2023-05-01 01:52:02 +1200

    Need help regarding crop

--- src/cosc202/andie/CropSelection.java
+++ src/cosc202/andie/CropSelection.java
@@ -16,5 +16,19 @@ public class CropSelection implements ImageOperation, java.io.Serializable {
         //Outcome: It crops, but repeating the process makes it... not work. It also kind of explodes on the third crop.
 
         //I think it has to do with the input taking from the original image pre-crop.
+        //I feel as if this code is way too simple. But what can I do to possibly fix it??? 
+        //It should work just like resize. But crop. But it doesn't...
+        //I've tried the way my Resize does things. Same outcome... and besides that other one is basically just conversion from
+        //an Image to BufferedImage. This doesn't need correction, as it is already a BufferedImage, but... huh...???????
+
+        /*Errors as of note:
+         * Caused on 1st crop: None
+         * Caused on 2nd crop: Returns 'must have an image opened'. Otherwise may or may not crop properly. 
+         * No idea why error message is triggered.
+         * Caused on 3rd crop: Exception in thread "AWT-EventQueue-0" java.awt.image.RasterFormatException: (x + width) is outside of Raster
+         * Hmm.
+         * 
+         * Undo: Does not return to previous sizes unlike Resize until returning to the first crop.
+         */
     }
 }
