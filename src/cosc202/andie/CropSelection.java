d9b78e0f2db795a30ede3d5456e75139348eed57
hasal314 <hasal314@student.otago.ac.nz>
2023-05-05 14:24:50 +1200

    Fix cropping/selection (not clean/commented)

--- src/cosc202/andie/CropSelection.java
+++ src/cosc202/andie/CropSelection.java
@@ -7,83 +7,104 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CropSelection implements ImageOperation, java.io.Serializable {
-    public CropSelection(){
+    private int x;
+    private int y;
+    private int w;
+    private int h;
 
+    public CropSelection(int posx, int posy, int width, int height) {
+        x = posx;
+        y = posy;
+        w = width;
+        h = height;
     }
-    public BufferedImage apply(BufferedImage input){
+
+    public BufferedImage apply(BufferedImage input) {
         System.out.println("For testing purposes: ");
         System.out.println("Width of the input image: " + input.getWidth());
         System.out.println("Height of the input image: " + input.getHeight());
 
-        
+        System.out.println("points: " + SelectionActions.originPoint.x + " " + SelectionActions.originPoint.y);
+
         System.out.println("Should select width: " + SelectionActions.selWidth);
         System.out.println("Should select height: " + SelectionActions.selHeight);
 
-        
-
-        BufferedImage inpCrop = input.getSubimage(SelectionActions.originPoint.x, SelectionActions.originPoint.y, SelectionActions.selWidth, SelectionActions.selHeight);
-        //'shares the same data array' 
-        //[pain]
+        BufferedImage inpCrop = input.getSubimage(x, y, w, h);
+        // 'shares the same data array'
+        // [pain]
         input = inpCrop;
-        
+
         System.out.println("Width of the changed input image: " + input.getWidth());
         System.out.println("Height of the changed input image: " + input.getHeight());
-        BufferedImage output = new BufferedImage(SelectionActions.selWidth, SelectionActions.selHeight, BufferedImage.TYPE_4BYTE_ABGR);
+        // BufferedImage output = new BufferedImage(SelectionActions.selWidth,
+        // SelectionActions.selHeight,
+        // BufferedImage.TYPE_4BYTE_ABGR);
 
+        // Graphics2D lay = output.createGraphics();
+        // lay.drawImage(inpCrop, 0, 0, null);
 
-        
-        Graphics2D lay = output.createGraphics();
-        lay.drawImage(inpCrop, 0, 0, null);
-        
-        lay.dispose();
-    
-        System.out.println("Width of the output image: " + output.getWidth());
-        System.out.println("Height of the output image: " + output.getHeight());
-        return output;
+        // lay.dispose();
 
-        //Outcome: It crops, but repeating the process makes it... not work. It also kind of explodes on the third crop.
+        // System.out.println("Width of the output image: " + output.getWidth());
+        // System.out.println("Height of the output image: " + output.getHeight());
+        return inpCrop;
 
-        //I think it has to do with the input taking from the original image pre-crop.
-        //I feel as if this code is way too simple. But what can I do to possibly fix it??? 
-        //It should work just like resize. But crop. But it doesn't...
-        //I've tried the way my Resize does things. Same outcome... and besides that other one is basically just conversion from
-        //an Image to BufferedImage. This doesn't need correction, as it is already a BufferedImage, but... huh...???????
+        // Outcome: It crops, but repeating the process makes it... not work. It also
+        // kind of explodes on the third crop.
 
-        /*Errors as of note:
+        // I think it has to do with the input taking from the original image pre-crop.
+        // I feel as if this code is way too simple. But what can I do to possibly fix
+        // it???
+        // It should work just like resize. But crop. But it doesn't...
+        // I've tried the way my Resize does things. Same outcome... and besides that
+        // other one is basically just conversion from
+        // an Image to BufferedImage. This doesn't need correction, as it is already a
+        // BufferedImage, but... huh...???????
+
+        /*
+         * Errors as of note:
          * Caused on 1st crop: None
-         * Caused on 2nd crop: Returns 'must have an image opened first'. Otherwise may or may not crop properly. 
+         * Caused on 2nd crop: Returns 'must have an image opened first'. Otherwise may
+         * or may not crop properly.
          * No idea why error message is triggered.
-         * Caused on 3rd crop: Exception in thread "AWT-EventQueue-0" java.awt.image.RasterFormatException: (x + width) is outside of Raster
+         * Caused on 3rd crop: Exception in thread "AWT-EventQueue-0"
+         * java.awt.image.RasterFormatException: (x + width) is outside of Raster
          * Hmm.
          * 
-         * Undo: Does not return to previous sizes unlike Resize until returning to the first crop.
+         * Undo: Does not return to previous sizes unlike Resize until returning to the
+         * first crop.
          * 
-         * It's odd. It should be working. The getSubImage function is basically THE function for cropping.
+         * It's odd. It should be working. The getSubImage function is basically THE
+         * function for cropping.
          * 
          * But... a possible solution is to go pixel by pixel paint...
          */
 
+        /*
+         * Testing results:
+         * Initial crop has the initial image's length and width.
+         * However, the second one has the print prompts go *twice,* sans the 'output
+         * height and width' messages. That's odd. That shouldn't happen...
+         * And for that matter, the input's length and width don't change, either, as
+         * expected.
+         */
 
-        /* Testing results:
-        * Initial crop has the initial image's length and width. 
-        However, the second one has the print prompts go *twice,* sans the 'output height and width' messages. That's odd. That shouldn't happen...
-        And for that matter, the input's length and width don't change, either, as expected.
-        */
-
-        /* EEK!!!!!
-
-        I'VE TRIED EVERYTHINGGGGGGGG!!!!!!!!!!
-
-        EVEN MAKING IT A SEPARATE ACTION SANS OPEN MENU! IT DOESN'T! WORK!!!!!
-
-        ...
-
-        I bet it only doesn't work on this.......
-
-        I've even tested the other mechanic that works similarly (resize) if it's because its in a method outside of actionPerformed - 
-        and yet, it worked perfectly fine.
-
-        WHAT'S GOING ON???
-        */
+        /*
+         * EEK!!!!!
+         * 
+         * I'VE TRIED EVERYTHINGGGGGGGG!!!!!!!!!!
+         * 
+         * EVEN MAKING IT A SEPARATE ACTION SANS OPEN MENU! IT DOESN'T! WORK!!!!!
+         * 
+         * ...
+         * 
+         * I bet it only doesn't work on this.......
+         * 
+         * I've even tested the other mechanic that works similarly (resize) if it's
+         * because its in a method outside of actionPerformed -
+         * and yet, it worked perfectly fine.
+         * 
+         * WHAT'S GOING ON???
+         */
     }
 }
--- src/cosc202/andie/SelectionActions.java
+++ src/cosc202/andie/SelectionActions.java
@@ -5,10 +5,8 @@ import java.util.prefs.Preferences;
 import java.awt.event.*;
 import javax.swing.*;
 
-
 import java.awt.FlowLayout;
 import java.awt.Point;
-
 
 public class SelectionActions {
     protected ArrayList<Action> actions;
@@ -20,20 +18,18 @@ public class SelectionActions {
      */
 
     protected static Point selFirst, selSecond;
-    protected static Point originPoint = new Point(0,0);
-    protected static int selWidth, selHeight; 
-
+    protected static Point originPoint = new Point(0, 0);
+    protected static int selWidth, selHeight;
 
     public SelectionActions() {
 
         Preferences prefs = Preferences.userNodeForPackage(Andie.class);
         Locale.setDefault(new Locale(prefs.get("language", "en"), prefs.get("country", "NZ")));
         ResourceBundle bundle = ResourceBundle.getBundle("TMessageBundle");
-        
-        
 
         actions = new ArrayList<Action>();
-        actions.add(new RectangularSelectionAction(bundle.getString("SelectPortion"), null, "Select a rectangular portion of the image", Integer.valueOf(KeyEvent.VK_M)));
+        actions.add(new RectangularSelectionAction(bundle.getString("SelectPortion"), null,
+                "Select a rectangular portion of the image", Integer.valueOf(KeyEvent.VK_M)));
     }
 
     /**
@@ -50,15 +46,15 @@ public class SelectionActions {
         ResourceBundle bundle = ResourceBundle.getBundle("TMessageBundle");
 
         JMenu selectMenu = new JMenu(bundle.getString("Selection"));
-        
-        for(Action action: actions) {
+
+        for (Action action : actions) {
             selectMenu.add(new JMenuItem(action));
         }
 
         return selectMenu;
     }
 
-    public class RectangularSelectionAction extends ImageAction implements MouseListener{
+    public class RectangularSelectionAction extends ImageAction implements MouseListener {
 
         private Point first;
         private Point second;
@@ -66,20 +62,20 @@ public class SelectionActions {
 
         RectangularSelectionAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
-            
+
         }
 
-        
-
-        public void actionPerformed(ActionEvent e){
-            //JLabel status = new JLabel("Select First Point");
+        public void actionPerformed(ActionEvent e) {
+            // JLabel status = new JLabel("Select First Point");
             target.addMouseListener(this);
             System.out.println("ML Instantiated and Set");
             System.out.println("Should be primed for selecting");
         }
 
-        public void mouseExited(MouseEvent e){}
-        public void mouseReleased(MouseEvent e){
+        public void mouseExited(MouseEvent e) {
+        }
+
+        public void mouseReleased(MouseEvent e) {
             second = e.getPoint();
             System.out.println("Second Point Clicked");
             System.out.println(second);
@@ -88,30 +84,34 @@ public class SelectionActions {
             SelectionActions.selSecond = second;
             target.removeMouseListener(this);
             System.out.println("Both Clicked. Opening menu of selection operations.");
-        
+
             openMenu();
         }
-        public void mousePressed(MouseEvent e){
+
+        public void mousePressed(MouseEvent e) {
             first = e.getPoint();
             System.out.println("First Point Clicked");
             System.out.println(first);
             firstCheck = true;
         }
-        public void mouseEntered(MouseEvent e){}
-        public void mouseClicked(MouseEvent e) {  
-        } 
 
-        private void openMenu(){
+        public void mouseEntered(MouseEvent e) {
+        }
 
-            if(selFirst.x < selSecond.x){
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        private void openMenu() {
+
+            if (selFirst.x < selSecond.x) {
                 SelectionActions.originPoint.x = selFirst.x;
                 SelectionActions.selWidth = selSecond.x - selFirst.x;
             } else {
                 SelectionActions.originPoint.x = selSecond.x;
                 SelectionActions.selWidth = selFirst.x - selSecond.x;
             }
-    
-            if(selFirst.y < selSecond.y){
+
+            if (selFirst.y < selSecond.y) {
                 SelectionActions.originPoint.y = selFirst.y;
                 SelectionActions.selHeight = selSecond.y - selFirst.y;
             } else {
@@ -125,24 +125,26 @@ public class SelectionActions {
             target.repaint();
             target.getParent().revalidate();
 
-            String[] options = {"Cancel", "Crop"};
-            int option = JOptionPane.showOptionDialog(null, "Select functions go here.", "Selection Operations Menu", JOptionPane.CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
-            
+            String[] options = { "Cancel", "Crop" };
+            int option = JOptionPane.showOptionDialog(null, "Select functions go here.", "Selection Operations Menu",
+                    JOptionPane.CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
+
             target.getImage().undo();
             target.repaint();
             target.getParent().revalidate();
 
             if (option == 1) {
-                target.getImage().apply(new CropSelection());
+                target.getImage().apply(new CropSelection((int) originPoint.getX(), (int) originPoint.getY(),
+                        SelectionActions.selWidth, SelectionActions.selHeight));
                 target.repaint();
                 target.getParent().revalidate();
-                
+
                 System.out.println("Width of the output image: " + target.getWidth());
                 System.out.println("Height of the output image: " + target.getHeight());
-            } 
-            selFirst = new Point(0,0);
-            selSecond = new Point(0,0);
-            originPoint = new Point(0,0);
+            }
+            selFirst = new Point(0, 0);
+            selSecond = new Point(0, 0);
+            originPoint = new Point(0, 0);
         }
 
     }
