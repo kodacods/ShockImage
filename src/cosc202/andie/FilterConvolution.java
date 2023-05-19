cbbae2aea1df1a9b87b56e1a27a1081dd73bf2f3
hasal314 <hasal314@student.otago.ac.nz>
2023-05-19 15:54:29 +1200

    fix filter convolution and stamping,
    need to fix filters not working on cropped images

--- src/cosc202/andie/FilterConvolution.java
+++ src/cosc202/andie/FilterConvolution.java
@@ -31,64 +31,69 @@ public class FilterConvolution {
         int height = input.getHeight();
         int width = input.getWidth();
 
-        // Apply naive image convolution, if there is an offset. If there is not an offset, then only apply the naive image convolution to the edges of the image.
+        // Apply naive image convolution, if there is an offset. If there is not an
+        // offset, then only apply the naive image convolution to the edges of the
+        // image.
         BufferedImage output = new BufferedImage(input.getColorModel(), input.copyData(null),
                 input.isAlphaPremultiplied(), null);
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                if (offset != 0 || (y < radius || y > height - radius || x < radius || x > width - radius)) {
-                float r = 0.0f;
-                float g = 0.0f;
-                float b = 0.0f;
-                for (int dy = -radius; dy <= radius; dy++) {
-                    for (int dx = -radius; dx <= radius; dx++) {
+                if (offset != 0 || (y < radius || y >= height - radius || x < radius || x >= width - radius)) {
+                    float r = 0.0f;
+                    float g = 0.0f;
+                    float b = 0.0f;
+                    for (int dy = -radius; dy <= radius; dy++) {
+                        for (int dx = -radius; dx <= radius; dx++) {
 
-                        int xValue = dx;
-                        int yValue = dy;
-                        if (x + xValue < 0 || x + xValue >= width - 1)
-                            xValue = 0;
-                        if (y + yValue < 0 || y + yValue >= height - 1)
-                            yValue = 0;
-                        float currentKernelValue = array[(dx + radius) + (((radius * 2) + 1) * (dy + radius))];
-                        Color currentColor = new Color(input.getRGB(x + xValue, y + yValue));
+                            int xValue = dx;
+                            int yValue = dy;
+                            if (x + xValue < 0 || x + xValue >= width - 1)
+                                xValue = 0;
+                            if (y + yValue < 0 || y + yValue >= height - 1)
+                                yValue = 0;
+                            float currentKernelValue = array[(dx + radius) + (((radius * 2) + 1) * (dy + radius))];
+                            Color currentColor = new Color(input.getRGB(x + xValue, y + yValue));
 
-                        float dr = currentColor.getRed() * currentKernelValue;
-                        float dg = currentColor.getGreen() * currentKernelValue;
-                        float db = currentColor.getBlue() * currentKernelValue;
-                        r += dr;
-                        g += dg;
-                        b += db;
+                            float dr = currentColor.getRed() * currentKernelValue;
+                            float dg = currentColor.getGreen() * currentKernelValue;
+                            float db = currentColor.getBlue() * currentKernelValue;
+                            r += dr;
+                            g += dg;
+                            b += db;
+                        }
                     }
-                }
-                r += offset;
-                g += offset;
-                b += offset;
-                if (r > 255)
-                    r = 255;
-                if (g > 255)
-                    g = 255;
-                if (b > 255)
-                    b = 255;
-                if (r < 0)
-                    r = 0;
-                if (g < 0)
-                    g = 0;
-                if (b < 0)
-                    b = 0;
-                Color outputColor = new Color(Math.round(r), Math.round(g), Math.round(b));
+                    r += offset;
+                    g += offset;
+                    b += offset;
+                    if (r > 255)
+                        r = 255;
+                    if (g > 255)
+                        g = 255;
+                    if (b > 255)
+                        b = 255;
+                    if (r < 0)
+                        r = 0;
+                    if (g < 0)
+                        g = 0;
+                    if (b < 0)
+                        b = 0;
+                    Color outputColor = new Color(Math.round(r), Math.round(g), Math.round(b));
 
-                output.setRGB(x, y, outputColor.getRGB());
+                    output.setRGB(x, y, outputColor.getRGB());
                 }
             }
         }
-        // if there is no offset, then apply java's ConvolveOp convolution to apply the convolution to the center of the image.
+        // if there is no offset, then apply java's ConvolveOp convolution to apply the
+        // convolution to the center of the image.
         if (offset == 0) {
-            Kernel k = new Kernel(1+(radius*2), 1+(radius*2), array);
-            BufferedImage convolvedImage = new BufferedImage(input.getColorModel(), input.copyData(null), input.isAlphaPremultiplied(), null);
-            ConvolveOp op = new ConvolveOp(k);
+            Kernel k = new Kernel(1 + (radius * 2), 1 + (radius * 2), array);
+            BufferedImage convolvedImage = new BufferedImage(input.getColorModel(),
+                    input.copyData(null), input.isAlphaPremultiplied(), null);
+            ConvolveOp op = new ConvolveOp(k, ConvolveOp.EDGE_ZERO_FILL, null);
             op.filter(input, convolvedImage);
             Graphics g = output.getGraphics();
-            g.drawImage(convolvedImage, 0, 0, null);
+            convolvedImage = convolvedImage.getSubimage(radius, radius, width - radius, height - radius);
+            g.drawImage(convolvedImage, radius, radius, null);
             g.dispose();
         }
         return output;
--- src/cosc202/andie/TransformActions.java
+++ src/cosc202/andie/TransformActions.java
@@ -16,7 +16,8 @@ import javax.swing.*;
  * </p>
  * 
  * <p>
- * The Transform menu encompasses the following tasks regarding image modification:
+ * The Transform menu encompasses the following tasks regarding image
+ * modification:
  * Rotation, reflection / flip and dilation / resize.
  * </p>
  * 
@@ -34,19 +35,25 @@ public class TransformActions {
      * </p>
      */
 
-    public TransformActions(){
+    public TransformActions() {
 
         Preferences prefs = Preferences.userNodeForPackage(Andie.class);
         Locale.setDefault(new Locale(prefs.get("language", "en"), prefs.get("country", "NZ")));
         ResourceBundle bundle = ResourceBundle.getBundle("TMessageBundle");
-        
+
         actions = new ArrayList<Action>();
-        actions.add(new ResizeTransformAction(bundle.getString("Resize"), null, "Resize image", Integer.valueOf(KeyEvent.VK_M)));
-        actions.add(new RotateAntiClock90TransformAction(bundle.getString("RotateAnticlockwise90"), null, "Rotate image anticlockwise by 90 degrees", Integer.valueOf(KeyEvent.VK_M)));
-        actions.add(new RotateClock90TransformAction(bundle.getString("RotateClockwise90"), null, "Rotate image clockwise by 90 degrees", Integer.valueOf(KeyEvent.VK_M)));
-        actions.add(new Rotate180TransformAction(bundle.getString("Rotate180"), null, "Rotate image by 180 degrees", Integer.valueOf(KeyEvent.VK_M)));
-        actions.add(new FlipVertTransformAction(bundle.getString("FlipVertical"), null, "Flip image vertically", Integer.valueOf(KeyEvent.VK_M)));
-        actions.add(new FlipHorzTransformAction(bundle.getString("FlipHorizontal"), null, "Flip image horizontally", Integer.valueOf(KeyEvent.VK_M)));
+        actions.add(new ResizeTransformAction(bundle.getString("Resize"), null, "Resize image",
+                Integer.valueOf(KeyEvent.VK_M)));
+        actions.add(new RotateAntiClock90TransformAction(bundle.getString("RotateAnticlockwise90"), null,
+                "Rotate image anticlockwise by 90 degrees", Integer.valueOf(KeyEvent.VK_M)));
+        actions.add(new RotateClock90TransformAction(bundle.getString("RotateClockwise90"), null,
+                "Rotate image clockwise by 90 degrees", Integer.valueOf(KeyEvent.VK_M)));
+        actions.add(new Rotate180TransformAction(bundle.getString("Rotate180"), null, "Rotate image by 180 degrees",
+                Integer.valueOf(KeyEvent.VK_M)));
+        actions.add(new FlipVertTransformAction(bundle.getString("FlipVertical"), null, "Flip image vertically",
+                Integer.valueOf(KeyEvent.VK_M)));
+        actions.add(new FlipHorzTransformAction(bundle.getString("FlipHorizontal"), null, "Flip image horizontally",
+                Integer.valueOf(KeyEvent.VK_M)));
         // TODO: translation
         actions.add(new StampAction("Stamp", null, "Stamp image onto current image", Integer.valueOf(KeyEvent.VK_M)));
     }
@@ -56,21 +63,21 @@ public class TransformActions {
      * Creates the menu containing said list of image transformation actions.
      * 
      * @return The transform menu UI content.
-     * </p>
+     *         </p>
      */
 
-     public JMenu createMenu(){
+    public JMenu createMenu() {
         Preferences prefs = Preferences.userNodeForPackage(Andie.class);
         Locale.setDefault(new Locale(prefs.get("language", "en"), prefs.get("country", "NZ")));
         ResourceBundle bundle = ResourceBundle.getBundle("TMessageBundle");
 
         JMenu transformMenu = new JMenu(bundle.getString("Transform"));
 
-        for(Action action: actions){
+        for (Action action : actions) {
             transformMenu.add(new JMenuItem(action));
         }
         return transformMenu;
-     }
+    }
 
     /**
      * <p>
@@ -80,16 +87,16 @@ public class TransformActions {
      * @see ResizeTransform
      */
 
-     public class ResizeTransformAction extends ImageAction{
+    public class ResizeTransformAction extends ImageAction {
         /**
          * <p>
          * Create a new resize action.
          * </p>
          * 
-         * @param name The name of the action (ignored if null).
-         * @param icon An icon to use to represent the action (ignored if null).
-         * @param desc A brief description of the action  (ignored if null).
-         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
          */
 
         ResizeTransformAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
@@ -103,13 +110,14 @@ public class TransformActions {
          * 
          * <p>
          * This method is called whenever the ResizeTransformAction is triggered.
-         * It prompts the user for a resize factor, then applys an appropriately sized {@link ResizeTransform}.
+         * It prompts the user for a resize factor, then applys an appropriately sized
+         * {@link ResizeTransform}.
          * </p>
          * 
          * @param e The event triggering this callback.
          */
 
-        public void actionPerformed(ActionEvent e){
+        public void actionPerformed(ActionEvent e) {
             if (!target.getImage().hasImage()) {
                 JOptionPane.showMessageDialog(null, "You need to open an image first!");
                 return;
@@ -121,7 +129,8 @@ public class TransformActions {
             // Pop-up dialog box to ask for the factor value.
             SpinnerNumberModel scaleModel = new SpinnerNumberModel(1.0, 0.0, 10.0, 0.1);
             JSpinner scaleSpinner = new JSpinner(scaleModel);
-            int option = JOptionPane.showOptionDialog(null, scaleSpinner, "Enter scale factor", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+            int option = JOptionPane.showOptionDialog(null, scaleSpinner, "Enter scale factor",
+                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
 
             // Check the return value from the dialog box.
             if (option == JOptionPane.CANCEL_OPTION) {
@@ -137,9 +146,9 @@ public class TransformActions {
             target.getParent().revalidate();
         }
 
-     }
+    }
 
-     /**
+    /**
      * <p>
      * Action to rotate an image 90 degrees anticlockwise.
      * </p>
@@ -147,30 +156,30 @@ public class TransformActions {
      * @see RotateAntiClock90Transform
      */
 
-     public class RotateAntiClock90TransformAction extends ImageAction{
+    public class RotateAntiClock90TransformAction extends ImageAction {
         /**
          * <p>
          * Create a new rotate anti-clockwise 90 degrees action.
          * </p>
          * 
-         * @param name The name of the action (ignored if null).
-         * @param icon An icon to use to represent the action (ignored if null).
-         * @param desc A brief description of the action  (ignored if null).
-         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
          */
         RotateAntiClock90TransformAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
 
-        public void actionPerformed(ActionEvent e){
+        public void actionPerformed(ActionEvent e) {
             target.getImage().apply(new RotateAntiClock90Transform());
             target.repaint();
             target.getParent().revalidate();
         }
 
-     }
+    }
 
-     /**
+    /**
      * <p>
      * Action to rotate an image 90 degrees clockwise.
      * </p>
@@ -178,28 +187,28 @@ public class TransformActions {
      * @see RotateClock90Transform
      */
 
-     public class RotateClock90TransformAction extends ImageAction{
+    public class RotateClock90TransformAction extends ImageAction {
         /**
          * <p>
          * Create a new rotate clockwise 90 degrees action.
          * </p>
          * 
-         * @param name The name of the action (ignored if null).
-         * @param icon An icon to use to represent the action (ignored if null).
-         * @param desc A brief description of the action  (ignored if null).
-         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
          */
         RotateClock90TransformAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
 
-        public void actionPerformed(ActionEvent e){
+        public void actionPerformed(ActionEvent e) {
             target.getImage().apply(new RotateClock90Transform());
             target.repaint();
             target.getParent().revalidate();
         }
 
-     }
+    }
 
     /**
      * <p>
@@ -209,21 +218,21 @@ public class TransformActions {
      * @see Rotate180Transform
      */
 
-     public class Rotate180TransformAction extends ImageAction{
-        //baller .
+    public class Rotate180TransformAction extends ImageAction {
+        // baller .
         Rotate180TransformAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
 
-        public void actionPerformed(ActionEvent e){
+        public void actionPerformed(ActionEvent e) {
             target.getImage().apply(new Rotate180Transform());
             target.repaint();
             target.getParent().revalidate();
         }
 
-     }
+    }
 
-     /**
+    /**
      * <p>
      * Action to flip an image vertically.
      * </p>
@@ -231,18 +240,18 @@ public class TransformActions {
      * @see FlipVertTransform
      */
 
-     public class FlipVertTransformAction extends ImageAction{
-        
+    public class FlipVertTransformAction extends ImageAction {
+
         FlipVertTransformAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
 
-        public void actionPerformed(ActionEvent e){ 
+        public void actionPerformed(ActionEvent e) {
             target.getImage().apply(new FlipVertTransform());
             target.repaint();
-            target.getParent().revalidate(); 
+            target.getParent().revalidate();
         }
-     }
+    }
 
     /**
      * <p>
@@ -252,18 +261,18 @@ public class TransformActions {
      * @see FlipHorzTransform
      */
 
-     public class FlipHorzTransformAction extends ImageAction{
-        
+    public class FlipHorzTransformAction extends ImageAction {
+
         FlipHorzTransformAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
 
-        public void actionPerformed(ActionEvent e){ 
+        public void actionPerformed(ActionEvent e) {
             target.getImage().apply(new FlipHorzTransform());
             target.repaint();
-            target.getParent().revalidate(); 
+            target.getParent().revalidate();
         }
-     }
+    }
 
     /**
      * <p>
@@ -272,8 +281,8 @@ public class TransformActions {
      * 
      * @see Stamp
      */
-     public class StampAction extends ImageAction implements MouseListener {
-        
+    public class StampAction extends ImageAction implements MouseListener {
+
         private int x = -1, y = -1;
         private BufferedImage stamp;
 
@@ -282,19 +291,20 @@ public class TransformActions {
          * Create a new stamp action.
          * </p>
          * 
-         * @param name The name of the action (ignored if null).
-         * @param icon An icon to use to represent the action (ignored if null).
-         * @param desc A brief description of the action  (ignored if null).
-         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
          */
         StampAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
 
         /**
-         * This param is called when the mouse is released. The 
+         * This param is called when the mouse is released. The
          * MouseListener is only added when an image is chosen and the
          * window is selected. It draws the image onto the current image.
+         * 
          * @param e mouseevent
          */
         public void mouseReleased(MouseEvent e) {
@@ -302,34 +312,43 @@ public class TransformActions {
             double positionScale = target.getZoom() / 100.0;
             this.x = (int) Math.round(p.x / positionScale);
             this.y = (int) Math.round(p.y / positionScale);
-            target.getImage().apply(new Stamp(stamp, x, y));  
+            target.getImage().apply(new Stamp(stamp, x, y));
             target.repaint();
-            target.getParent().revalidate();   
+            target.getParent().revalidate();
         }
 
-        public void mouseExited(MouseEvent e) {}
-        public void mousePressed(MouseEvent e) {}
-        public void mouseEntered(MouseEvent e) {}
-        public void mouseClicked(MouseEvent e) {}
+        public void mouseExited(MouseEvent e) {
+        }
+
+        public void mousePressed(MouseEvent e) {
+        }
+
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        public void mouseClicked(MouseEvent e) {
+        }
 
         public void disable() {
             target.removeMouseListener(this);
+            target.addMouseListener(target);
         }
-        
+
         /**
          * This method shows a file chooser to the user to pick which image
          * to use as the stamp. It them informs the user of how to use
          * the stamp, and applies the stamp action when the mouse is
          * clicked. When the escape key is pressed, it stops listening for
          * mouse clicks.
+         * 
          * @param e action event
          */
-        public void actionPerformed(ActionEvent e){ 
+        public void actionPerformed(ActionEvent e) {
             if (!target.getImage().hasImage()) {
                 JOptionPane.showMessageDialog(null, "You need to open an image first!");
                 return;
             }
-            
+
             JFileChooser fileChooser = new JFileChooser();
             int result = fileChooser.showOpenDialog(target);
 
@@ -348,8 +367,9 @@ public class TransformActions {
                             disable();
                         }
                     };
-                    target.registerKeyboardAction(action, stroke, JComponent.WHEN_IN_FOCUSED_WINDOW);   
-                    target.addMouseListener(this);       
+                    target.registerKeyboardAction(action, stroke, JComponent.WHEN_IN_FOCUSED_WINDOW);
+                    target.addMouseListener(this);
+                    target.removeMouseListener(target);
                 } catch (Exception ex) {
                     // If file is not valid, give the user an error message and restart the file
                     // opening process.
@@ -359,5 +379,5 @@ public class TransformActions {
                 }
             }
         }
-     }
+    }
 }
